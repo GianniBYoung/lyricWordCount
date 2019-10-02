017c465a66334dad7c19664671b03adb45480c0e
GianniBYoung <43043456+GianniBYoung@users.noreply.github.com>
2019-10-02 13:04:56 -0500

    beautified

--- lyricWordCount.py
+++ lyricWordCount.py
@@ -6,31 +6,35 @@ from trim import extraTrim
 genius = lyricsgenius.Genius("Your Access Token Here")
 
 print("Please provide an artist to search for:\n")
-givenArtist= input()
+givenArtist = input()
 print("Please provide an song to search for:\n\n")
-givenSong= input()
+givenSong = input()
 
 
 song = genius.search_song(givenSong, givenArtist, get_full_info=False)
-song.save_lyrics(filename='lyrics',extension='txt',verbose=False,overwrite=True)
+song.save_lyrics(filename='lyrics', extension='txt',
+                 verbose=False, overwrite=True)
 
 extraTrim("lyrics.txt")
-lyricsList= open("lyrics.txt").readlines()
-lyrics= ""
-print("\n*********************************************************************************\n"+givenSong.title()+" by "+ givenArtist.title() +" Lyrics:\n")
+lyricsList = open("lyrics.txt").readlines()
+lyrics = ""
+print("\n*********************************************************************************\n" +
+      givenSong.title() + " by " + givenArtist.title() + " Lyrics:\n")
 punctuation = """!"#$%&'()*+,./:;<=>?@[\]^_`{|}~"""
 for line in lyricsList:
-    lyrics+= "".join(line)
+    lyrics += "".join(line)
     print(line.strip('\n'))
     for c in punctuation:
-        lyrics= lyrics.replace(c,"")
+        lyrics = lyrics.replace(c, "")
 
-print("\n*********************************************************************************\n\n"+ "Occurences of each unique word:\n")
+print("\n*********************************************************************************\n\n" +
+      "Occurences of each unique word:\n")
 
-dict = {word: lyrics.count(word)+1 for word in split(r'[,\s]\s*', lyrics.lower())}
+dict = {word: lyrics.count(
+    word) + 1 for word in split(r'[,\s]\s*', lyrics.lower())}
 if '' in dict:
     del dict['']
 sorted_dict = sorted(dict.items(), key=lambda x: x[1])
-sorted_dict= sorted_dict[::-1]
+sorted_dict = sorted_dict[::-1]
 for x in range(len(sorted_dict)):
     print(sorted_dict[x])
--- trim.py
+++ trim.py
@@ -1,9 +1,9 @@
 def extraTrim(fileName):
-    f=open(fileName,"r")
-    text=f.readlines()
-    f=open(fileName,"w")
+    f = open(fileName, "r")
+    text = f.readlines()
+    f = open(fileName, "w")
 
     for line in text:
-        if ord(line[0])!=91 and ord(line[0])!=40:
+        if ord(line[0]) != 91 and ord(line[0]) != 40:
             f.write(line)
     f.close()
