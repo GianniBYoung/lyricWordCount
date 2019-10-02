f3abefee20ef3347b51a8b9b26beda6c184de637
Gianni Young <43043456+GianniBYoung@users.noreply.github.com>
2019-10-01 19:42:28 -0500

    Add files via upload
    ordered output and addressed hyphens

--- wordcount.py
+++ wordcount.py
@@ -18,20 +18,23 @@ extraTrim("lyrics.txt")
 lyricsList= open("lyrics.txt").readlines()
 lyrics= ""
 print("\n*********************************************************************************\n"+givenSong.title()+" by "+ givenArtist.title() +" Lyrics:\n")
-
+punctuation = """!"#$%&'()*+,./:;<=>?@[\]^_`{|}~"""
 for line in lyricsList:
     lyrics+= "".join(line)
     print(line.strip('\n'))
-    # remove punctuation to reduce issues with the word occurence
-    for c in string.punctuation:
+    for c in punctuation:
         lyrics= lyrics.replace(c,"")
 
-print("\n*********************************************************************************\n\n"+ "\nOccurences of each unique word:\n\n")
+print("\n*********************************************************************************\n\n"+ "Occurences of each unique word:\n")
 
 dict = {word: lyrics.count(word)+1 for word in split(r'[,\s]\s*', lyrics.lower())}
+if '' in dict:
+    del dict['']
+sorted_dict = sorted(dict.items(), key=lambda x: x[1])
+sorted_dict= sorted_dict[::-1]
+for x in range(len(sorted_dict)):
+    print(sorted_dict[x])
 
-for key in dict:
-    output= str(key)+": "+ str(dict[key])
-    print(output)
-# order descending
+
 #issues with single char (x gon give it to ya)
+#deal with '-'
